016ae12a1dbff4865b33c8d568ae32e775f49e6f
hp <hp@DESKTOP-4OQ6L4K>
2026-06-25 03:53:49 +0300

    Session05

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -18,18 +18,87 @@ namespace Gym.Controllers
             var members = await _memberService.GetAllMembersAsync(ct);
             return View(members);
         }
+        #region Create
         public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
         public async Task<IActionResult> CreateMember(CreateMemberViewModel model, CancellationToken ct)
         {
+
             if (!ModelState.IsValid)
             {
-                return View(nameof(Create),model);
+                return View(nameof(Create), model);
             }
-            await _memberService.CreateMemberAsync(model, ct);
+            var result = await _memberService.CreateMemberAsync(model, ct);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Member created successfully.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed to create member.";
+            }
             return RedirectToAction(nameof(Index));
         }
+        #endregion
+
+        #region Details
+
+        public async Task<IActionResult> MemberDetails(int id, CancellationToken ct)
+        {
+            var member = await _memberService.GetMemberDetailsByIdAsync(id, ct);
+            if (member == null)
+            {
+                TempData["ErrorMessage"] = "Member not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+
+        public async Task<IActionResult> HealthRecordDetails(int id, CancellationToken ct)
+        {
+            var healthRecord = await _memberService.GetHealthRecordByMemberIdAsync(id, ct);
+            if (healthRecord == null)
+            {
+                TempData["ErrorMessage"] = "Health record not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(healthRecord);
+        }
+
+
+        #endregion
+
+        #region delete
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id,CancellationToken ct)
+        {
+            var member = await _memberService.GetMemberDetailsByIdAsync(id, ct);
+            if (member == null)
+            {   TempData["ErrorMessage"] = "Member not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id, CancellationToken ct)
+        {
+            var result = await _memberService.DeleteMemberAsync(id, ct);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Member deleted successfully.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed to delete member.";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
+
     }
 }
